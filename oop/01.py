5a493426c68c1927bc005084349282ce570c1816
fj111111 <1251694418@qq.com>
2019-01-15 03:45:04 -0800

    gx

--- oop/01.py
+++ oop/01.py
@@ -1,6 +1,36 @@
 '''
-
+创建一个学生类
 '''
-class Student():
 
-     pass
+
+# 定义一个空类
+class Student():
+    # 一个空类必须有代码
+    # 用pass站位
+    pass
+
+
+# 定义一个对象
+mingyue = Student()
+
+
+# 定义一个Python学生的类
+class PythonStudent():
+    name = None
+    age = 10
+    course = 'Python'
+
+    def doHomework(self):
+        self.name
+        self.age
+        self.course
+        print('i 在做作业')
+        # 函数末尾推荐使用reutrn
+        return None
+
+
+# 实例化
+lili = PythonStudent()
+print(lili.name)
+print(lili.age)
+lili.doHomework()
